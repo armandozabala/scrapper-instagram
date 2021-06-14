e0a9515e22878c440d851e242f2288756ed5a534
armando zabala <armandozabala@gmail.com>
2021-06-14 17:26:05 -0500

    first commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express  = require('express');
 const Routes = require('./routes/routes');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors())
 
 app.get('/',(req,resp) => {
     resp.json({ msg: 'holis'})
