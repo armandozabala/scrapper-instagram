eeeebd2fc300b463ec5d30da19cf3365569a9193
armando zabala <armandozabala@gmail.com>
2021-06-14 17:39:06 -0500

    first commit

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,13 +1,19 @@
 const { Router } = require('express');
-const instagram = require('./../instagram');
+const instagram = require('../instagram');
 
 const routes = Router();
 
 routes.get('/profile/:id', async (req, res) => {
 
     let id = req.params.id
-    const resp = await instagram.getProfile(id);
-     res.send(resp)
+   
+    try{
+        const resp = await instagram.getProfile(id);
+        res.send(resp)
+    }catch(err){
+        console.log(err)
+    }
+   
  });
 
 
